0bc493d28ad1a203289a48b97d37b932b012d768
Adrian Hossner Manjaro <adhossner@gmail.com>
2025-12-15 14:06:17 +0100

    create tables in db

--- backend/databaseCreate.js
+++ backend/databaseCreate.js
@@ -1,1 +1,15 @@
+import dotenv from 'dotenv'
 
+
+
+cosole.log(process.env.DATABSE_URL)
+
+async function createUsersTable() {
+    try {
+	await client.connect();
+	// create the table here
+    } catch (error) {
+    	console.log("Error occured when trying to create database tables");
+    }
+    
+};
