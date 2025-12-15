0b69b565945d5a91ce2a5cdb206eb3d263d0b330
Adrian Hossner Manjaro <adhossner@gmail.com>
2025-12-15 13:51:36 +0100

    make backend better

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 // index.js
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+// import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
